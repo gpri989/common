aec84bece5aba3739f20faed6f0f130fcca7ee3e
Sonal Singh <sonal.singh.19993@gmail.com>
2020-11-02 22:33:22 +0530

    X01BD: Xtended Bringup
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The MSM-Xtended Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X01BD.mk
+    $(LOCAL_DIR)/xtended_X01BD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X01BD-userdebug
+    xtended_X01BD-userdebug
--- xtended_X01BD.mk
+++ xtended_X01BD.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The MSM-Xtended Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,14 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xtended stuff
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X01BD
+PRODUCT_NAME := xtended_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_X01BDA
@@ -31,3 +31,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Build fingerprint
 BUILD_FINGERPRINT := "asus/WW_X01BD/ASUS_X01BD_1:10/QKQ1/17.2018.2004.424-20200506:user/release-keys"
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Official MSM-Xtended
+XTENDED_BUILD_TYPE := OFFICIAL
+
+# Maintainer
+XTENDED_MAINTAINER := 🔥SonalSingh18🔥
+
+# Gapps
+ifeq ($(SHIP_GAPPS),true)
+$(call inherit-product-if-exists, vendor/google/gms/gms-vendor.mk)
+endif
