6e53b64719657142d0a9798aa271a8fcc3efb41d
gpri989 <gpriyanshu903@gmail.com>
2020-12-04 07:36:51 +0000

    Xtended -> ROS

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2020 The MSM-Xtended Project
+# Copyright (C) 2018-2020 RevengeOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,9 +13,9 @@ INITIAL_COPYRIGHT_YEAR=2020
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-XTENDED_ROOT="${MY_DIR}/../../.."
+REVENGEOS_ROOT="${MY_DIR}/../../.."
 
-HELPER="${XTENDED_ROOT}/vendor/xtended/build/tools/extract_utils.sh"
+HELPER="${REVENGEOS_ROOT}/vendor/revengeos/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -23,7 +23,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${XTENDED_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${REVENGEOS_ROOT}" true
 
 # Copyright headers and guards
 write_headers "X00TD X01BD"
@@ -37,7 +37,7 @@ write_footers
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${XTENDED_ROOT}" false
+    setup_vendor "${DEVICE}" "${VENDOR}" "${REVENGEOS_ROOT}" false
 
     # Copyright headers and guards
     write_headers
